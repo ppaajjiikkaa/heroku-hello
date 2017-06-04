ae9528e72f23da50f6a3ed5c26ae6ccde944e1a7
ppaajjiikkaa <ppaajjiikkaa@seznam.cz>
2017-06-04 15:05:14 +0200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 import cv2
+import base64
+import numpy as np
 
 def index(request):
-    ret = cv2.__version__
+    ret = "<html><body>"
+    ret += "<img src='request.POST.get("pokus","")'>
+    ret += </body></html>
     #ret = request.POST.get("pokus","")
+    
     return HttpResponse(ret)
 
 # Create your views here.

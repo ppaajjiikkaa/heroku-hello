9e23fa9b080872696db4ec51e6fc5f2c895debab
ppaajjiikkaa <ppaajjiikkaa@seznam.cz>
2017-06-04 14:42:05 +0200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 def index(request):
-    return HttpResponse(request.GET.get("pokus",""))
+    return HttpResponse(request.POST.get("pokus",""))
 
 # Create your views here.

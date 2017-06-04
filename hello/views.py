88a68791d58ba1bf561a28339785ad6c914c17c5
ppaajjiikkaa <ppaajjiikkaa@seznam.cz>
2017-06-04 14:54:50 +0200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+import cv2
 
 def index(request):
-    return HttpResponse(request.POST.get("pokus",""))
+    ret = cv2.__version__
+    #ret = request.POST.get("pokus","")
+    return HttpResponse(ret)
 
 # Create your views here.

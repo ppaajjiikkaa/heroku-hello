e7aacffd4615a27bb8f91fbdde8b93ae915b56d2
ppaajjiikkaa <ppaajjiikkaa@seznam.cz>
2017-06-04 21:42:06 +0200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
-import cv2
-import base64
-import numpy as np
+from django.http import HttpResponse, JsonResponse
+#import cv2
+#import base64
+#import numpy as np
 import aio
+import json
 
 def join_list(list_to_join):
     return (', '.join(str(x) for x in list_to_join))
 
 def index(request):
-    ret = "<html><body>"
+    #ret = "<html><body>"
     imgbase = request.POST.get("pokus","")
-    ret += "<img src='"+imgbase+"'>"
-    ret += "<p>"+join_list(aio.detect(imgbase))+"</p>"
-    ret += "</body></html>"
+    #ret += "<img src='"+imgbase+"'>"
+    #ret += "<p>"+join_list(aio.detect(imgbase))+"</p>"
+    #ret += "</body></html>"
     #ret = request.POST.get("pokus","")
     
-    return HttpResponse(ret)
+    #return HttpResponse(ret)
+    return JsonResponse(json.dumps(aio.detect(imgbase)))
 
 # Create your views here.

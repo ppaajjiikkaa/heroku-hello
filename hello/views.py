4d17ad06bf1f03d8a2d62f5236241b3221a73faf
ppaajjiikkaa <ppaajjiikkaa@seznam.cz>
2017-06-04 20:44:08 +0200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -5,11 +5,14 @@ import base64
 import numpy as np
 import aio
 
+def join_list(list_to_join):
+    return (', '.join(str(x) for x in list_to_join))
+
 def index(request):
     ret = "<html><body>"
     imgbase = request.POST.get("pokus","")
     ret += "<img src='"+imgbase+"'>"
-    ret += "<p>"+join(aio.detect(imgbase))+"</p>"
+    ret += "<p>"+join_list(aio.detect(imgbase))+"</p>"
     ret += "</body></html>"
     #ret = request.POST.get("pokus","")
     

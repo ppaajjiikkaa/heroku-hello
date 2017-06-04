75f1bddf93b050efc86b4e62667d3a53308f24fa
ppaajjiikkaa <ppaajjiikkaa@seznam.cz>
2017-06-04 15:43:47 +0200

    Update settings.py

--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CSRF_TRUSTED_ORIGINS = (
+CSRF_TRUSTED_ORIGINS = [
     'pajika.wz.cz',
-)
+]
 
 #bezpečnostní nastavení pro POST - pak to ale bude voser
 #'django.middleware.csrf.CsrfViewMiddleware',

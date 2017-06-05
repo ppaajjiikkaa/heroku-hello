1ff699cf698b75f339542e0d0320024cc0d5d147
ppaajjiikkaa <ppaajjiikkaa@seznam.cz>
2017-06-05 16:27:00 +0200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -11,7 +11,7 @@ def join_list(list_to_join):
 
 def index(request):
     #ret = "<html><body>"
-    imgbase = request.data
+    imgbase = request.stream
     #ret += "<img src='"+imgbase+"'>"
     #ret += "<p>"+join_list(aio.detect(imgbase))+"</p>"
     #ret += "</body></html>"

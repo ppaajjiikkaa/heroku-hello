688d856058809927e784d563144f640ffc94fa28
ppaajjiikkaa <ppaajjiikkaa@seznam.cz>
2017-06-05 22:42:10 +0200

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -10,14 +10,7 @@ def join_list(list_to_join):
     return (', '.join(str(x) for x in list_to_join))
 
 def index(request):
-    #ret = "<html><body>"
-    imgbase = str(request.body)
-    #ret += "<img src='"+imgbase+"'>"
-    #ret += "<p>"+join_list(aio.detect(imgbase))+"</p>"
-    #ret += "</body></html>"
-    #ret = request.POST.get("pokus","")
-    
-    #return HttpResponse(ret)
+    imgbase = str(request.body).decode("utf-8")
     #ret = JsonResponse(json.dumps(aio.detect(imgbase)), safe=False)
     #ret = HttpResponse(json.dumps(aio.detect(imgbase)))
     ret = HttpResponse(aio.detect(imgbase))

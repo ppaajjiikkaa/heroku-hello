24a227d43a884c2dcba522a741b71214cdd4d17d
ppaajjiikkaa <ppaajjiikkaa@seznam.cz>
2017-06-14 12:35:50 +0200

    Create views.py

--- hello/views.py
+++ hello/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import aio
 import json
-#import cloudinary
+import cloud
 
 def join_list(list_to_join):
     return (', '.join(str(x) for x in list_to_join))
 
 def index(request):
     imgbase = request.body.decode("utf-8")
-    #cloudinary.cloudupload(imgbase)
+    cloud.cloudupload(imgbase)
     ret = JsonResponse(json.dumps(aio.detect(imgbase)), safe=False)
        
     return ret
